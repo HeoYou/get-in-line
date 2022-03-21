0072144dfecc0c2a49f0368a9ab5eb8282483c9d
HeoYou <lov21y_@naver.com>
2022-03-22 00:04:43 +0900

    Add #3 - domain

---
+++ src/main/java/com/heoyou/getinline/domain/Event.java
@@ -0,0 +1,24 @@
+package com.heoyou.getinline.domain;
+
+
+import com.heoyou.getinline.constant.EventStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Event {
+    private Long id;
+
+    private Long placeId;
+    private String eventName;
+    private EventStatus eventStatus;
+    private LocalDateTime eventStartDatetime;
+    private LocalDateTime eventEndDatetime;
+    private Integer currentNumberOfPeople;
+    private Integer capacity;
+    private String memo;
+
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+}
